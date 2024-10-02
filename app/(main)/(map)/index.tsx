c0c051bf54dd099d12c4441fcc44fb3656a0f09e
Xavier Huix Trenco <xavierhuixtrenco@MacBook-Pro-de-Xavier.local>
2024-10-02 19:45:41 +0200

    add dist and amplify

--- app/(main)/(map)/index.tsx
+++ app/(main)/(map)/index.tsx
@@ -171,7 +171,7 @@ export default function MapScreen() {
 							zoom: 15,
 							altitude: 10,
 						}}
-						googleMapsApiKey="AIzaSyDvzNpAZfrhv5GN8JyGWet9dcjnfoL8y-Y"
+						googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}
 						options={{
 							disableDefaultUI: true,
 						}}
